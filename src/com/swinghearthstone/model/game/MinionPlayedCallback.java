5760ce9406ff1245b9a5bd5b58e991a8829a6c2d
Martin Lehmann <lehmar11@student.westerdals.no>
2015-12-30 01:21:51 +0100

    Make MinionPlayedCallback a functional interface.

--- src/com/swinghearthstone/model/game/MinionPlayedCallback.java
+++ src/com/swinghearthstone/model/game/MinionPlayedCallback.java
@@ -2,6 +2,7 @@ package com.swinghearthstone.model.game;
 
 import com.swinghearthstone.model.game.minion.Minion;
 
+@FunctionalInterface
 public interface MinionPlayedCallback
 {
     void apply(Minion minion);
